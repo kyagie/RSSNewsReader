5416b3da9e6d3585a9a9faaed04ccb921e90b7b5
Arthur Kyagulanyi <akyagulanyi@tayariafrica.com>
2021-01-22 09:46:54 +0300

    New Activities

---
+++ app/src/main/java/com/example/rssnewsreader/NewsHeadlinesActivity.java
@@ -0,0 +1,14 @@
+package com.example.rssnewsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsHeadlinesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_headlines);
+    }
+}
