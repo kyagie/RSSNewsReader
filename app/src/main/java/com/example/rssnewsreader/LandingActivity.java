7ddad5a45c408411467562a70ef1b83221675bef
Arthur Kyagulanyi <akyagulanyi@tayariafrica.com>
2021-01-26 11:38:45 +0300

    URL

--- app/src/main/java/com/example/rssnewsreader/LandingActivity.java
+++ app/src/main/java/com/example/rssnewsreader/LandingActivity.java
@@ -2,13 +2,52 @@ package com.example.rssnewsreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LandingActivity extends AppCompatActivity {
+    private TextInputLayout filledTextField;
+    private Button containedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        containedButton = (Button) findViewById(R.id.containedButton);
+        containedButton.setOnClickListener(this::mainactivity);
+
     }
+    private boolean isValidate(){
+        filledTextField = (TextInputLayout) findViewById(R.id.filledTextField);
+        String URL = filledTextField.getEditText().toString();
+//        return  Patterns.WEB_URL.matcher(URL).matches();
+        return true;
+    }
+
+    public void submitURL(){
+        if (isValidate()){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else {
+            Toast.makeText(getApplicationContext(),"Invalid URL", Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+
+    public void mainactivity(View view){
+        if(view == containedButton){
+            submitURL();
+        }
+    }
+
+
 }
